b85fc0abf45968fd54f8a13c1011dfac0a30dd77
lawtancool <26829131+lawtancool@users.noreply.github.com>
2020-04-04 20:31:49 -0700

    fix password rehashing issue

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,6 +7,8 @@ if ( [ -z "${MOSQUITTO_USERNAME}" ] || [ -z "${MOSQUITTO_PASSWORD}" ] ); then
   exit 1
 fi
 
+# remove existing passwordfile to avoid rehashing existing password and breaking it
+rm -f passwordfile || true
 # create mosquitto passwordfile
 touch passwordfile
 mosquitto_passwd -b passwordfile $MOSQUITTO_USERNAME $MOSQUITTO_PASSWORD
